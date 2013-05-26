0ce967dda6044bcab396be91a4142b5a1e7afeac
George Redinger <george@georgeredinger.com>
2013-05-25 18:07:12 -0700

    add smoothie chart

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var PORT = 12345;
 var HOST = '192.168.1.100';
 var dgram = require('dgram');
 var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
+  , io = require('socket.io').listen(app,{ log: false })
   , fs = require('fs')
 
 app.listen(8083);
@@ -16,16 +16,29 @@ server.on('listening', function () {
 server.bind(PORT, HOST);
 
 function handler (req, res) {
-    fs.readFile(__dirname + '/index.html',
-                function (err, data) {
-                    if (err) {
-                        res.writeHead(500);
-                        return res.end('Error loading index.html');
-                    }
+	if(req.url == "/smoothie.js") {
+		fs.readFile(__dirname + '/smoothie.js',
+								function (err, data) {
+									if (err) {
+										res.writeHead(500);
+										return res.end('Error loading index.html');
+									}
 
-                    res.writeHead(200);
-                    res.end(data);
-                });
+									res.writeHead(200);
+									res.end(data);
+								});
+	} else {
+		fs.readFile(__dirname + '/index.html',
+								function (err, data) {
+									if (err) {
+										res.writeHead(500);
+										return res.end('Error loading index.html');
+									}
+
+									res.writeHead(200);
+									res.end(data);
+								});
+	}
 }
 
 io.sockets.on('connection', function (socket) {
@@ -33,7 +46,7 @@ io.sockets.on('connection', function (socket) {
         //  console.log(remote.address + ':' + remote.port +' - ' + message);
           var accel =  {x:0,y:0,z:0};
           accel = JSON.parse(message);
-          console.log(accel);
+        //  console.log(accel);
 
           socket.emit("accel",accel);
 
